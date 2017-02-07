ef0558b96abfbf575a0d003b76636741756b0e20
Daniel Porrey <Daniel.Porrey@hotmail.com>
2017-02-07 07:57:41 -0600

    Added Sample

--- examples/sizeof/sizeof.ino
+++ examples/sizeof/sizeof.ino
@@ -17,5 +17,51 @@
 // see http://www.gnu.org/licenses/.
 //
 
-#include "EEPROM-Storage.h"
+#if defined(SPARK)
+#define word short
+#endif
 
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.print("The size of byte is "); Serial.print(sizeof(byte)); Serial.println(".");
+  Serial.print("The size of char is "); Serial.print(sizeof(char)); Serial.println(".");
+  Serial.print("The size of bool is "); Serial.print(sizeof(bool)); Serial.println(".");
+  Serial.print("The size of boolean is "); Serial.print(sizeof(boolean)); Serial.println(".");
+  Serial.print("The size of short is "); Serial.print(sizeof(short)); Serial.println(".");
+	Serial.print("The size of word is "); Serial.print(sizeof(word)); Serial.println(".");
+  Serial.print("The size of int is "); Serial.print(sizeof(int)); Serial.println(".");
+  Serial.print("The size of long is "); Serial.print(sizeof(long)); Serial.println(".");
+  Serial.print("The size of float is "); Serial.print(sizeof(float)); Serial.println(".");
+  Serial.print("The size of double is "); Serial.print(sizeof(double)); Serial.println(".");
+  Serial.println();
+
+  Serial.print("The size of unsigned char is "); Serial.print(sizeof(unsigned char)); Serial.println(".");
+  Serial.print("The size of unsigned byte is "); Serial.print(sizeof(unsigned byte)); Serial.println(".");
+  Serial.print("The size of unsigned short is "); Serial.print(sizeof(unsigned short)); Serial.println(".");
+	Serial.print("The size of unsigned word is "); Serial.print(sizeof(unsigned word)); Serial.println(".");
+  Serial.print("The size of unsigned int is "); Serial.print(sizeof(unsigned int)); Serial.println(".");
+  Serial.print("The size of unsigned long is "); Serial.print(sizeof(unsigned long)); Serial.println(".");
+  Serial.println();
+
+  Serial.print("The size of uint8_t is "); Serial.print(sizeof(uint8_t)); Serial.println(".");
+  Serial.print("The size of uint16_t is "); Serial.print(sizeof(uint16_t)); Serial.println(".");
+  Serial.print("The size of uint32_t is "); Serial.print(sizeof(uint32_t)); Serial.println(".");
+  Serial.print("The size of uint64_t is "); Serial.print(sizeof(uint64_t)); Serial.println(".");
+  Serial.println();
+
+  Serial.print("The size of int8_t is "); Serial.print(sizeof(int8_t)); Serial.println(".");
+  Serial.print("The size of int16_t is "); Serial.print(sizeof(int16_t)); Serial.println(".");
+  Serial.print("The size of int32_t is "); Serial.print(sizeof(int32_t)); Serial.println(".");
+  Serial.print("The size of int64_t is "); Serial.print(sizeof(int64_t)); Serial.println(".");
+  Serial.println();
+}
+
+void loop()
+{
+  // ***
+  // *** Delay 2 seconds.
+  // ***
+  delay(2000);
+}
